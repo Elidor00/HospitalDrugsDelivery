ba03c065a403d8600f9cd1fcaa59dfa3796b6f54
Elidor00 <filippo.bartolini9@gmail.com>
2020-06-01 11:11:12 +0200

    Fixed controller avoid obstacles

--- controllers/controller_pioneer3dx.py
+++ controllers/controller_pioneer3dx.py
@@ -1,16 +1,21 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, LED, Motor
+from controller import Robot
 
 MAX_SENSOR_NUMBER = 16
 MAX_SPEED = 5.24
 MAX_SENSOR_VALUE = 1024
 MIN_DISTANCE = 1.0
-WHEEL_WEIGHT_THRESHOLD = 200
+WHEEL_WEIGHT_THRESHOLD = 100
 
-sensors = [ 
-    (150, 0), (200, 0), (300, 0), (600, 0),
-    (0, 600), (0, 300),  (0, 200),  (0, 150),
+FORWARD = 0
+LEFT = 1
+RIGHT = 2
+state = FORWARD
+
+sensors = [
+    (150, 0),  (200, 0),  (300, 0),  (600, 0),
+    (0, 600),  (0, 300),  (0, 200),  (0, 150),
     (0, 0),    (0, 0),    (0, 0),    (0, 0),
     (0, 0),    (0, 0),    (0, 0),    (0, 0)
 ]
@@ -26,35 +31,23 @@ rightMotor = robot.getMotor('right wheel')
 
 # lista vuota per i sensori di posizione
 distance_sensors = []
-distance_sensor_names = []
-
-FORWARD = 0
-LEFT = 1
-RIGHT = 2
-state = FORWARD
 
 #inizializzo la lista di posizione con i loro nomi
 for i in range(MAX_SENSOR_NUMBER):
-    name_sensor = "so"
-    distance_sensor_names.append(name_sensor + str(i))
-
-print(*distance_sensor_names)
-# inizializzo distance_sensors con i sensori
-for i in range(16):
-    distance_sensors.append(robot.getDistanceSensor(distance_sensor_names[i]))
+    name_sensor = "so" + str(i)
+    distance_sensors.append(robot.getDistanceSensor(name_sensor))
     distance_sensors[i].enable(TIME_STEP)  # TIME_STEP dovrebbe dire ogni quanto
     # aggiornare il valore dei sensori
-
-print(*distance_sensors)
 
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 
-left_speed = 0.0
-right_speed = 0.0
+speed = [0.0, 0.0]
 
-leftMotor.setVelocity(left_speed)
-rightMotor.setVelocity(right_speed)
+# speed[1] = motore sx
+# speed[0] = motore dx
+leftMotor.setVelocity(speed[0])
+rightMotor.setVelocity(speed[1])
 
 wheel_weight_total = [0.0, 0.0]
 
@@ -64,11 +57,9 @@ while robot.step(TIME_STEP) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-    left_speed = 0
-    right_speed = 0
+    speed = [0.0, 0.0]
     wheel_weight_total = [0.0, 0.0]
 
-    speed_modifier = 0.0
     for i in range(16):
         sensor_value = distance_sensors[i].getValue()
 
@@ -78,74 +69,44 @@ while robot.step(TIME_STEP) != -1:
             distance = 5.0 * (1.0 - (sensor_value / MAX_SENSOR_VALUE))
             if distance < MIN_DISTANCE:
                 speed_modifier = 1 - (distance / MIN_DISTANCE)
-                #print("Speed_modifier MODIFICATO " + str(speed_modifier))
             else:
                 speed_modifier = 0.0
         for j in range(2):
-            #print("Speed_modifier IN FOR " + str(speed_modifier))
             wheel_weight_total[j] += sensors[i][j] * speed_modifier
 
     # 1 = sx, 0 = dx
     if state == FORWARD:
         if wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD:
-            right_speed = 0.7 * MAX_SPEED
-            left_speed = -0.7 * MAX_SPEED
+            speed[0] = 0.7 * MAX_SPEED
+            speed[1] = -0.7 * MAX_SPEED
             state = LEFT
         elif wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD:
-            right_speed = -0.7 * MAX_SPEED
-            left_speed = 0.7 * MAX_SPEED
+            speed[0] = -0.7 * MAX_SPEED
+            speed[1] = 0.7 * MAX_SPEED
             state = RIGHT
         else:
-            right_speed = MAX_SPEED
-            left_speed = MAX_SPEED
+            speed[0] = MAX_SPEED
+            speed[1] = MAX_SPEED
     elif state == LEFT:
         if wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD \
                 or wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD:
-            right_speed = 0.7 * MAX_SPEED
-            left_speed = -0.7 * MAX_SPEED
+            speed[0] = 0.7 * MAX_SPEED
+            speed[1] = -0.7 * MAX_SPEED
         else:
-            right_speed = MAX_SPEED
-            left_speed = MAX_SPEED
+            speed[0] = MAX_SPEED
+            speed[1] = MAX_SPEED
             state = FORWARD
     elif state == RIGHT:
         if wheel_weight_total[0] > WHEEL_WEIGHT_THRESHOLD \
                 or wheel_weight_total[1] > WHEEL_WEIGHT_THRESHOLD:
-            right_speed = -0.7 * MAX_SPEED
-            left_speed = 0.7 * MAX_SPEED
+            speed[0] = -0.7 * MAX_SPEED
+            speed[1] = 0.7 * MAX_SPEED
         else:
-            right_speed = MAX_SPEED
-            left_speed = MAX_SPEED
+            speed[0] = MAX_SPEED
+            speed[1] = MAX_SPEED
             state = FORWARD
 
-
-    '''        
-    print(*distance_sensor_values)
-    # Process sensor data here.
-    # detect obstacles
-
-    right_obstacles = distance_sensor_values[5] > 50.0 \
-                      or distance_sensor_values[6] > 50.0 or distance_sensor_values[7] > 50.0
-    left_obstacles = distance_sensor_values[0] > 50.0 or distance_sensor_values[1] > 50.0 \
-                     or distance_sensor_values[2] > 50.0
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-
-    # initialize motor speeds
-    # mi serve per settare la velocità delle ruote a seconda
-    # se ci sia l'ostacolo a dx o sx
-    leftSpeed = 0.5 * MAX_SPEED
-    rightSpeed = 0.5 * MAX_SPEED
-
-    # se l'ostacolo è a sx, giro a dx e viceversa
-    if left_obstacles:
-        leftSpeed += 0.8 * MAX_SPEED
-        rightSpeed -= 0.8 * MAX_SPEED
-    elif right_obstacles:
-        leftSpeed -= 0.8 * MAX_SPEED
-        rightSpeed += 0.8 * MAX_SPEED
-    '''
-    leftMotor.setVelocity(left_speed)
-    rightMotor.setVelocity(right_speed)
+    leftMotor.setVelocity(speed[0])
+    rightMotor.setVelocity(speed[1])
 
 # Enter here exit cleanup code.
